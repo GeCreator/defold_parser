31320b49cf03d7a0115bb5edcede682d342aebc2
gecreator <ge-rom@mail.ru>
2025-01-27 17:23:56 +0400

    cleanup

--- defold_parser.lua
+++ defold_parser.lua
@@ -1,25 +1,22 @@
 local offset = 0
 local file = ''
+local extract_dict
 
-local function trim(str)
-  return str:match("^%s*(.-)%s*$")
-end
+local function trim(str) return str:match("^%s*(.-)%s*$") end
 
-local function is_array(tbl)
-  return tbl[1] ~= nil
-end
+local function is_array(tbl) return tbl[1] ~= nil end
 
 ---@return string|nil
-local function _char()
+local function get_char()
   offset = offset + 1
   local c = file:sub(offset, offset)
   if c == '' then return nil end
   return c
 end
 
-local function _skip_spaces()
+local function skip_spaces()
   while 1 do
-    local c = _char()
+    local c = get_char()
     if c ~= ' ' and c ~= '\t' then
       offset = offset - 1
       return
@@ -27,13 +24,13 @@ local function _skip_spaces()
   end
 end
 
-local obj = {}
 ---@return string
-function obj:extract_key()
-  _skip_spaces()
+function extract_key()
+  skip_spaces()
   local result = ''
   while 1 do
-    local c = _char()
+    local c = get_char()
+    assert(c, "extract key error")
     if c:match('[a-zA-Z_]+') then
       result = result .. c
     else
@@ -44,20 +41,20 @@ function obj:extract_key()
 end
 
 ---@return string
-function obj:extract_scalar()
-  _skip_spaces()
+function extract_scalar()
+  skip_spaces()
   local result = ''
-  local c = _char()
-  if c == '"' then -- parse oneline/multiline text
+  local c = get_char()
+  if c == '"' then -- parse text (oneline/multiline)
     local skip_quote = false
     while 1 do
-      c = _char()
+      c = get_char()
       if c == nil then return result end
       if c == '"' and not skip_quote then
-        _skip_spaces()
-        if _char() == '"' then
+        skip_spaces()
+        if get_char() == '"' then
           offset = offset - 1
-          result = result .. "\n" .. self:extract_scalar()
+          result = result .. "\n" .. extract_scalar()
           return result
         else
           offset = offset - 1
@@ -72,7 +69,7 @@ function obj:extract_scalar()
   else -- parse oneline value (const/number)
     offset = offset - 1
     while 1 do
-      c = _char()
+      c = get_char()
       if c ~= " " and c ~= "\t" and c ~= nil then
         result = result .. c
       else
@@ -83,21 +80,21 @@ function obj:extract_scalar()
 end
 
 ---@return string|table
-function obj:extract_value()
-  _skip_spaces()
-  local c = _char()
-  if c == ':' then return obj:extract_scalar() end
-  if c == '{' then return obj:extract_dict() end
+function extract_value()
+  skip_spaces()
+  local c = get_char()
+  if c == ':' then return extract_scalar() end
+  if c == '{' then return extract_dict() end
   error("file format error")
 end
 
 ---@return table
-function obj:extract_dict()
-  _skip_spaces()
+extract_dict = function()
+  skip_spaces()
   local result = {}
   while 1 do
-    local key = obj:extract_key()
-    local value = obj:extract_value()
+    local key = extract_key()
+    local value = extract_value()
     if result[key] ~= nil then
       if not is_array(result[key]) then
         result[key] = { result[key] }
@@ -106,8 +103,8 @@ function obj:extract_dict()
     else
       result[key] = value
     end
-    _skip_spaces()
-    local c = _char()
+    skip_spaces()
+    local c = get_char()
     if c == "}" or c == nil then
       return result
     else
@@ -116,21 +113,24 @@ function obj:extract_dict()
   end
 end
 
-local export = {}
+-----------------------------------
+---@param text string
+local function parse(text)
+  offset = 0
+  file = text
+  return extract_dict()
+end
 ---@meta
 ---@param path string
-function export:parse_file(path)
+local function parse_file(path)
   local f = io.open(path, 'r')
   local rows = {}
   assert(f, ("read file error: %s"):format(path))
   for v in f:lines() do if v then table.insert(rows, trim(v)) end end
-  return self:parse(table.concat(rows, ' '))
+  return parse(table.concat(rows, ' '))
 end
 
-function export:parse(text)
-  offset = 0
-  file = text
-  return obj:extract_dict()
-end
-
-return export
+return {
+  parse_file = parse_file,
+  parse = parse
+}
